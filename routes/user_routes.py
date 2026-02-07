b9c5314868625964125c1cd4cf6ca9ec45d60353
SanikaMJ2005 <4mh23cs131@gmail.com>
2026-02-07 11:49:10 +0530

    hello

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -2,21 +2,21 @@ from fastapi import APIRouter
 from sqlalchemy.orm import Session
 from fastapi import Depends
 from db import get_db
+from models import User
 from repositories.User_repo import UserRepo
 from schemas.User_schemas import UserSchema
-
-
 router = APIRouter()
 
 
 @router.post("/signup")
 def signup(user: UserSchema, db: Session = Depends(get_db)):
-        user_repo = UserRepo(db)
-        new_user = User(email=user.email, password=user.password)
-        user_repo.add_user(new_user)
-        return {"message": "User created successfully"}
+        
+    user_repo = UserRepo(db)
+    # Convert Pydantic schema to SQLAlchemy model
+    db_user = User(email=user.email, password=user.password)
+    user_repo.add_user(db_user)
+    return {"message": "User signed up successfully"}
 
 @router.post("/login")
 def login():
-    return {"message": "User logged in successfully"} 
-
+    return {"message": "User logged in successfully"}from fastapi import APIRouter
